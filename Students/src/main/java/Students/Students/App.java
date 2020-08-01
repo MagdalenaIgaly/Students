777feb3b5f61b7a048eb1269fe93ed66239ede82
Magdalena Igaly <magdalena.igaly@gmail.com>
2020-08-01 17:00:07 +0200

    Initial commit

---
+++ Students/src/main/java/Students/Students/App.java
@@ -0,0 +1,13 @@
+package Students.Students;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
